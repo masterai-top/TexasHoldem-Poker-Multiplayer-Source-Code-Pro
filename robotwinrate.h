cdae21d4ccdc62361ac927b91d2ff69e764309e4
德州扑克源代码_德州游戏源码_德州扑克源码开发_聊天通讯软件 <develop@deeptexas.ai>
2026-08-06 19:58:14 +0800

    Add files via upload

---
+++ robotwinrate.h
@@ -0,0 +1,14 @@
+#pragma once
+
+namespace game
+{
+    class GameRoot;
+
+    namespace logic
+    {
+        namespace roomlogic
+        {
+            void SetRobotWinRate(void const *p, GameRoot *root);
+        }
+    }
+}
